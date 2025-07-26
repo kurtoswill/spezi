824c7f311520e02f12defa279327108e1ff1ea46
Kurt Oswill <kurtoswillmc@gmail.com>
2025-07-26 21:17:32 +0800

    feat: footer

---
+++ components/Footer.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+
+export const Footer = () => {
+    return (
+        <footer className="mt-16">
+            <div className="flex gap-5 items-center justify-end">
+                <Image src="/icons/fb.svg" alt="fb"
+                       width={30} height={30}
+                />
+                <Image src="/icons/ig.svg" alt="fb"
+                       width={30} height={30}
+                />
+                <Image src="/icons/linkedin.svg" alt="fb"
+                       width={30} height={30}
+                />
+            </div>
+
+            <hr
+                className="border-black border-1 my-10"
+            />
+
+            <div className="flex justify-between font-thin">
+                <div className="flex gap-5">
+                    <p>Terms & Conditions</p>
+                    <p>Privacy Policy</p>
+                </div>
+
+                <p>2025 Spezi All Rights Reserved.</p>
+            </div>
+        </footer>
+    );
+};
