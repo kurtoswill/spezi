bf62ec426cba36148ee42bf131ccb5845b2370a9
legnspice <nilestristan@gmail.com>
2025-07-27 21:30:55 +0800

    added content

---
+++ components/Instructions.tsx
@@ -0,0 +1,9 @@
+export const Instructions = () => {
+  return (
+    <>
+      <h2>How Spezi Works in Your Browser </h2>
+    </>
+  );
+};
+
+export default Instructions;
---
+++ components/Landing.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./ui/button";
+
+export const Landing = () => {
+  return (
+    <header className="mt-30">
+      <div>Speak English Professionally and Confidently</div>
+      <p>
+        Built for English as a Second Language (ESL) professionals, Spezi gives
+        you real-time, accent-aware feedback to help you communicate clearly in
+        global meetings.
+      </p>
+      <Button>Download Extension</Button>
+      <Button>Watch Demo</Button>
+      <p>Trusted by professionals across 20+ countries </p>
+      <figure></figure>
+      <figure></figure>
+    </header>
+  );
+};
